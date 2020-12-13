b32984dcbd86d6015e5cc8147b3ffba7d49a0299
setia_k <setiakagung15@gmail.com>
2020-12-13 12:51:07 +0700

    Add LED Level

--- Arduino Code/plantmonitoring.ino
+++ Arduino Code/plantmonitoring.ino
@@ -5,13 +5,37 @@
 // Make json variables
 const size_t capacity = JSON_OBJECT_SIZE(3);
 DynamicJsonDocument doc(capacity);
-// Variables
-int soil_sensor = A0;
+// Pins
+// LED Pins
+int led_red = 7;
+int led_ylw = 6;
+int led_grn = 5;
+// Sensor Pins
 int dhtPin = 8;
+int soil_sensor = A0;
+// Variables
 float humidity, temperature;
 int soilMoisture;
+int threshold_lwr = 300;
+int threshold_upr = 450;
 // Declare DHT pin and type
 DHT dht(dhtPin, DHTTYPE);
+//
+int ledLevel(int soilMoistureValue)
+{
+    if (soilMoistureValue < threshold_lwr)
+    {
+        return 1;
+    }
+    else if (soilMoistureValue >= threshold_lwr && soilMoistureValue <= threshold_upr)
+    {
+        return 2;
+    }
+    else if (soilMoistureValue > threshold_upr)
+    {
+        return 3;
+    }
+}
 //
 void setup()
 {
@@ -35,7 +59,27 @@ void loop()
     // Print Json
     serializeJson(doc, Serial);
     Serial.println();
-
+    //
+    switch (ledLevel(soilMoisture))
+    {
+    case 1:
+        digitalWrite(led_grn, HIGH);
+        digitalWrite(led_ylw, LOW);
+        digitalWrite(led_red, LOW);
+        break;
+    case 2:
+        digitalWrite(led_grn, LOW);
+        digitalWrite(led_ylw, HIGH);
+        digitalWrite(led_red, LOW);
+        break;
+    case 3:
+        digitalWrite(led_grn, LOW);
+        digitalWrite(led_ylw, LOW);
+        digitalWrite(led_red, HIGH);
+        break;
+    default:
+        break;
+    }
     // Delay
     delay(3000);
 }
